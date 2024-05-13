cb2f3e6256c41c8c57bf7bb5730569ecb9cd85cb
Suraj Vishwakarma <surajvishwakarma@protonmail.com>
2024-05-14 00:51:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import user from './user.json';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -8,6 +8,59 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'my-angular-app';
+  users = user;
+
+  editUser(user: any) {
+    // Logic to handle edit action, for example, redirecting to an edit page
+    console.log('Editing user:', user);
+    (<HTMLInputElement>document.getElementById('name')).value = user.name;
+    (<HTMLInputElement>document.getElementById('email')).value = user.email;
+    (<HTMLInputElement>document.getElementById('City')).value = user.city;
+    (<HTMLInputElement>document.getElementById('zip')).value = user.zip;
+    (<HTMLInputElement>document.getElementById('age')).value = user.age;
+    // Add your logic here, for example, redirecting to edit page with user id
+    // this.router.navigate(['/edit-user', user.id]);
+    //AJAX call to get user asp api
+    // fetch('https://localhost:44390/api/user/' + user.id).then(response => {
+    //   return response.json();
+    // }).then(data => {
+    //   console.log(data);
+    // }).catch(err => {
+    //   console.log(err);
+    // });
+  }
+
+  deleteUser(user: any) {
+    // Logic to handle edit action, for example, redirecting to an edit page
+    console.log('Deleting user:', user);
+    // Add your logic here, for example, redirecting to edit page with user id
+    // this.router.navigate(['/edit-user', user.id]);
+    //AJAX call to delete user asp api
+  }
+
+  saveUser() {
+    // Logic to handle edit action, for example, redirecting to an edit page
+    let name = (<HTMLInputElement>document.getElementById('name')).value;
+    let email = (<HTMLInputElement>document.getElementById('email')).value;
+    let city = (<HTMLInputElement>document.getElementById('City')).value;
+    let zip = (<HTMLInputElement>document.getElementById('zip')).value;
+    let age = (<HTMLInputElement>document.getElementById('age')).value;
+  
+    let obj = {
+      "name": name,
+      "email": email,
+      "city": city,
+      "zip": zip,
+      "age": age
+    }
+
+    console.log('Save user:', obj);
+    // Add your logic here, for example, redirecting to edit page with user id
+    // this.router.navigate(['/edit-user', user.id]);
+    //AJAX call to delete user asp api
+  }
+
 }
